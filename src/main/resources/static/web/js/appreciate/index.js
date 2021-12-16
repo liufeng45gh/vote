cca1c07b2db2221874af165e54c44005817810b1
liufx <liufx@yiche.com>
2021-12-16 15:25:24 +0800

    re launch

--- src/main/resources/static/web/js/appreciate/index.js
+++ src/main/resources/static/web/js/appreciate/index.js
@@ -1,42 +1,42 @@
 $(document).ready(function(){
     $(".vote-btn").click(function () {
-        layer.msg('投票已结束!', {icon: 5});
-        return;
-//        var appreciateId = $(this).parent().attr("objectId");
-//        var url = "/vote/submit";
-//        var data_send = {};
-//        data_send.appreciateId = appreciateId;
-//
-//        var count_request =$.ajax({
-//         type: 'post',
-//         url: url,
-//         data: data_send,
-//         dataType: 'json'
-//        });
-//
-//        count_request.fail(function( jqXHR, textStatus ) {
-//            if(jqXHR.status==401){
-//               openWxLogin();
-//            }
-//
-//            if(jqXHR.status==410){
-//               layer.msg('重复投票!', {icon: 5});
-//            }
-//        });
-//
-//        count_request.done(function(data) {
-//
-//            layer.msg('投票成功!', {icon: 1});
-//           setTimeout(function(){
-//              var lineLink = window.location.href;
-//               if(lineLink.indexOf("?") != -1)
-//               {
-//                   lineLink = lineLink.split("?")[0];
-//                   console.log(lineLink);
-//               }
-//             window.location.href = lineLink+"?random=" + Math.random();
-//           },5);
-//        });
+//        layer.msg('投票已结束!', {icon: 5});
+//        return;
+        var appreciateId = $(this).parent().attr("objectId");
+        var url = "/vote/submit";
+        var data_send = {};
+        data_send.appreciateId = appreciateId;
+
+        var count_request =$.ajax({
+         type: 'post',
+         url: url,
+         data: data_send,
+         dataType: 'json'
+        });
+
+        count_request.fail(function( jqXHR, textStatus ) {
+            if(jqXHR.status==401){
+               openWxLogin();
+            }
+
+            if(jqXHR.status==410){
+               layer.msg('重复投票!', {icon: 5});
+            }
+        });
+
+        count_request.done(function(data) {
+
+            layer.msg('投票成功!', {icon: 1});
+           setTimeout(function(){
+              var lineLink = window.location.href;
+               if(lineLink.indexOf("?") != -1)
+               {
+                   lineLink = lineLink.split("?")[0];
+                   console.log(lineLink);
+               }
+             window.location.href = lineLink+"?random=" + Math.random();
+           },5);
+        });
     });
 
 });
