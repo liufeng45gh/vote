4cb8bc52d1798241eff47a8dc0eaf22c01572bc2
liufx <liufx@yiche.com>
2022-01-27 15:05:24 +0800

    resources

--- src/main/java/com/lucifer/controller/web/IndexController.java
+++ src/main/java/com/lucifer/controller/web/IndexController.java
@@ -3,8 +3,10 @@ package com.lucifer.controller.web;
 import com.lucifer.config.ImageConfig;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -18,5 +20,11 @@ public class IndexController {
     @GetMapping("/rule")
     public String rule(HttpServletRequest request){
         return "/web/index/rule";
+    }
+
+    @GetMapping("/resources")
+    @ResponseBody
+    public Map<String,String> resources(){
+        return ImageConfig.resource;
     }
 }
